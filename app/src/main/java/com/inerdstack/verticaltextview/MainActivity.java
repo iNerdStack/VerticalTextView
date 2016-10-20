041e10c23719ebcca05c3b11716078188ae303d3
iNerdStack <wjnovember@qq.com>
2016-10-20 18:29:33 +0800

    VerticalTextView

--- app/src/main/java/com/inerdstack/verticaltextview/MainActivity.java
+++ app/src/main/java/com/inerdstack/verticaltextview/MainActivity.java
@@ -5,9 +5,16 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    MyTextView myTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myTextView = (MyTextView) findViewById(R.id.my_text);
+        myTextView.setText("测试哈奥垃圾费阿里山的风景阿里上飞机爱上当减肥拉丝粉案例看是否");
+        myTextView.setMaxLines(2);
+        myTextView.setTextSize(12);
     }
 }
