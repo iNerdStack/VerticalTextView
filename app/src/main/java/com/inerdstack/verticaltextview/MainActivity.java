a92d7e735be5c14ffb64cb6b26fb82a535b2fc63
iNerdStack <wjnovember@qq.com>
2016-10-24 11:48:48 +0800

    v1.0.0

--- app/src/main/java/com/inerdstack/verticaltextview/MainActivity.java
+++ app/src/main/java/com/inerdstack/verticaltextview/MainActivity.java
@@ -1,21 +1,26 @@
 package com.inerdstack.verticaltextview;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    MyTextView myTextView;
+    VerticalTextView myTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myTextView = (MyTextView) findViewById(R.id.my_text);
-        myTextView.setText("测试哈asf说法是否打算暗室逢灯阿萨斯发顺丰asdf asS fa");
-        myTextView.setMaxLines(2);
-        myTextView.setTextSize(24);
-        myTextView.setLineSpacingMultiplier(1.3f);
+        myTextView = (VerticalTextView) findViewById(R.id.my_text);
+//        myTextView.setText("测试哈asf阿斯蒂芬阿发是打发收到发生发顺丰的阿斯蒂芬阿斯顿发送到发送到分公司地方大个地方是打发个地方官去问他关心的是啊阿斯蒂芬阿萨德发生的发顺丰大概二个是分开就是安徽案发后奥斯卡按时给对方看晚会if和 华山路发生 是啊是试哈asf阿斯蒂芬阿发是打发收到发生发顺丰的阿斯蒂芬阿斯顿发送到发送到分公司地方大个地方是打发个地方官去问他关心的是啊阿斯蒂芬阿萨德发生的发顺丰大概二个是分开就是安徽案发后奥斯卡按时给对方看晚会if和 华山路发生 是啊是试哈asf阿斯蒂芬阿发是打发收到发生发顺丰的阿斯蒂芬阿斯顿发送到发送到分公司地方大个地方是打发个地方官去问他关心的是啊阿斯蒂芬阿萨德发生的发顺丰大概二个是分开就是安徽案发后奥斯卡按时给对方看晚会if和 华山路发生 是啊是试哈asf阿斯蒂芬阿发是打发收到发生发顺丰的阿斯蒂芬阿斯顿发送到发送到分公司地方大个地方是打发个地方官去问他关心的是啊阿斯蒂芬阿萨德发生的发顺丰大概二个是分开就是安徽案发后奥斯卡按时给对方看晚会if和 华山路发生 是啊是试哈asf阿斯蒂芬阿发是打发收到发生发顺丰的阿斯蒂芬阿斯顿发送到发送到分公司地方大个地方是打发个地方官去问他关心的是啊阿斯蒂芬阿萨德发生的发顺丰大概二个是分开就是安徽案发后奥斯卡按时给对方看晚会if和 华山路发生 是啊是试哈asf阿斯蒂芬阿发是打发收到发生发顺丰的阿斯蒂芬阿斯顿发送到发送到分公司地方大个地方是打发个地方官去问他关心的是啊阿斯蒂芬阿萨德发生的发顺丰大概二个是分开就是安徽案发后奥斯卡按时给对方看晚会if和 华山路发生 是啊是试哈asf阿斯蒂芬阿发是打发收到发生发顺丰的阿斯蒂芬阿斯顿发送到发送到分公司地方大个地方是打发个地方官去问他关心的是啊阿斯蒂芬阿萨德发生的发顺丰大概二个是分开就是安徽案发后奥斯卡按时给对方看晚会if和 华山路发生 是啊是试哈asf阿斯蒂芬阿发是打发收到发生发顺丰的阿斯蒂芬阿斯顿发送到发送到分公司地方大个地方是打发个地方官去问他关心的是啊阿斯蒂芬阿萨德发生的发顺丰大概二个是分开就是安徽案发后奥斯卡按时给对方看晚会if和 华山路发生 是啊是");
+////        myTextView.setMaxLines(20);
+//        myTextView.setTextSize(24);
+//        myTextView.setLineSpacingMultiplier(0.3f);
+//        myTextView.setWidth(250);
+//        myTextView.setHeight(150);
+//        myTextView.setMaxLines(3);
+//        myTextView.setTextColor(ContextCompat.getColor(this, android.R.color.white));
     }
 }
